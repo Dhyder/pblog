b87b3cedb3b949ebfbf0d0fda3e1939e2a3eb245
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-11-15 12:39:31 +0300

    updated config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+import os 
+
+class Config:
+  SECRET_KEY= os.environ.get("SECRET_KEY")
+  DATABASE_URL='postgresql+psycopg2://postgres:787898@localhost/cielito'
+  UPLOADED_PHOTOS_DEST = "app/static"
+  SQLALCHEMY_TRACK_MODIFICATIONS = True
+  # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:787898@localhost/cielito"
+
+  # email configurations
+  MAIL_SERVER='smtp.gmail.com'
+  MAIL_PORT= 465
+  MAIL_USE_SSL=True
+  MAIL_USE_TLS= False
+  MAIL_USERNAME= os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD= os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    
+  SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:787898@localhost/cielito_test"
+
+class DevConfig(Config):
+  SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:787898@localhost/cielito"
+  DEBUG = True
+  ENV = 'development'
+
+config_options = {
+  "development": DevConfig,
+  "production": ProdConfig,
+  "test": TestConfig
+}
